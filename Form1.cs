b21ef3cfa2616c1f7b92defa942895925db8cdf4
nyj001012 <nyj001012@gmail.com>
2019-05-08 00:03:44 +0900

    first commit

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 실습과제1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //상수 선언
+            const double PI = 3.14;
+
+            //반지름 입력
+            double iRadius = int.Parse(txtBox1.Text);
+
+            //면적 계산
+            double area = PI * iRadius * iRadius;
+
+            //결과 표시
+            lblResult.Text = area.ToString();
+        }
+    }
+}
